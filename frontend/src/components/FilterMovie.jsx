24204261aad9a8f76e69536271d48f2105b22a38
francesco <francescodicorpo1992@hotmail.com>
2024-05-07 23:37:58 +0200

    feat: create filter movie component

---
+++ frontend/src/components/FilterMovie.jsx
@@ -0,0 +1,9 @@
+const FilterMovie = () => {
+  return ( 
+    <div className="mx-auto border-2 border-green-600 my-8 lg:mb-16">
+      <h1>filter</h1>
+    </div>
+  );
+}
+ 
+export default FilterMovie;
